e6710c1e6908ab8a3af81de771db0ba3965dbfae
Samuel <samuelrodiguez@hotmail.com>
2022-10-13 10:21:46 -0500

    actualizacion back

---
+++ MicSecurityService/src/test/java/com/example/MicSecurityService/MicSecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicSecurityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicSecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicSecurityServiceApplication.class, args);
+	}
+
+}
---
+++ MicSecurityService/src/test/java/com/example/MicSecurityService/MicSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicSecurityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicSecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
