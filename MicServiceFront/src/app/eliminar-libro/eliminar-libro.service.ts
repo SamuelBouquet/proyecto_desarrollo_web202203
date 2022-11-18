6cbf852cd623ac0fc61b7770d716085790b29518
Pablo Bright <64916681+pbright23@users.noreply.github.com>
2022-11-18 16:21:34 -0500

    q23er4
    5123456231456

--- MicServiceFront/src/app/eliminar-libro/eliminar-libro.service.ts
+++ MicServiceFront/src/app/eliminar-libro/eliminar-libro.service.ts
@@ -10,8 +10,6 @@ export class EliminarLibroService {
   constructor(private http: HttpClient) { }
 
   eliminarLibro(book: Book){
-    // console.log(book)
-    
     const params = new HttpParams()
       .set('id', book.id)
     const headers = new HttpHeaders()
@@ -20,5 +18,3 @@ export class EliminarLibroService {
   }
 
 }
-//Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJBZG1pbiIsInN1YiI6InRlc3RAdGVzdC5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjY4NjQ2MTIwLCJleHAiOjE2Njg2NDY3MjB9.1x7PDTFPTjnyGIaYdH15ysnr7kMy_56gMRNe6bXRqyt0ymzQwOTtMSJIg12fhTJZgDhPAceE6kahiz_45vDCcA
-//Bearer eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJBZG1pbiIsInN1YiI6InRlc3RAdGVzdC5jb20iLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwiaWF0IjoxNjY4NjQ2MTIwLCJleHAiOjE2Njg2NDY3MjB9.1x7PDTFPTjnyGIaYdH15ysnr7kMy_56gMRNe6bXRqyt0ymzQwOTtMSJIg12fhTJZgDhPAceE6kahiz_45vDCcA
